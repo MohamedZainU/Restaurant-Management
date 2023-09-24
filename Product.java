81096bfb379ac0f13904eb0173eb61597cb094cc
MohamedZainU <104151148+MohamedZainU@users.noreply.github.com>
2023-09-24 14:02:01 +0530

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,11 @@
+public class Product{
+    String foodName;
+    int price;
+    int quantity;
+    
+    public Product(String foodName,int price,int quantity){
+        this.foodName = foodName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
